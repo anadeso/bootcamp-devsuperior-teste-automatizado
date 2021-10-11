fd2aedada829a222d992874e3a32bf0a89ad495b
anadeso <anadeso90@gmail.com>
2021-10-11 15:49:38 -0300

    Implementando test deposito

---
+++ src/tests/entities/AccountTests.java
@@ -0,0 +1,23 @@
+package tests.entities;
+
+import entities.Account;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountTests {
+
+    @Test
+    public void depositShouldIncreaseBalanceWhenPositiveAmount() {
+
+        // Arrange
+        double amount = 200.0;
+        double expectedValue = 196.0;
+        Account account = new Account(1L, 0.0);
+
+        // Act
+        account.deposit(amount);
+
+        // Assert
+        Assertions.assertEquals(expectedValue, account.getBalance());
+    }
+}
